3f27cee493984e236516d513ee8e169d9eed5748
piggy-devil <muupaza@gmail.com>
2021-09-18 12:08:14 +0700

    Setup Vue3 Tailwind Laravel

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./storage/framework/views/*.php",
+    "./resources/**/*.{blade.php,js,vue}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
